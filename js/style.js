acdd777e8c30a8e53ce69a3ecf009a5fd3107953
marcostavaresf <marcosfreire.tel@outlook.com>
2018-06-19 23:14:13 -0300

    increasing the responsivit of the nav menu, still not working

---
+++ js/style.js
@@ -0,0 +1,10 @@
+// let header = document.getElementById('header')
+// let navbar = document.getElementById('navbar');
+// let hero = document.querySelector('.hero');
+
+
+// // navbar.addEventListener('click', ()=> {
+
+// //     document.querySelectorAll('.menu-princ').
+
+// // });
